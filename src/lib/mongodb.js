f6dfc5fbc1db288ea15291933eabeee9b306680c
Paundra Yudha Dharmawan <paundra461@gmail.com>
2024-08-16 15:39:09 +0700

    .

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,10 +1,6 @@
 import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
 
 let client;
 let clientPromise;
@@ -16,13 +12,13 @@ if (!process.env.MONGODB_URI) {
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable to preserve the value across module reloads
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+    client = new MongoClient(uri);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable
-  client = new MongoClient(uri, options);
+  client = new MongoClient(uri);
   clientPromise = client.connect();
 }
 
