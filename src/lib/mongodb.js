942721411b58ce320848cf3a7ebb82647db20ee2
paundrayudhad <155907525+paundrayudhad@users.noreply.github.com>
2024-08-16 14:40:31 +0700

    Update mongodb.js

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -2,20 +2,22 @@ import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
 const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useNewUrlParser: true,  // Ini masih diperlukan
+  // useUnifiedTopology: true, // Hapus baris ini
 };
 
 let client;
 let clientPromise;
 
 if (process.env.NODE_ENV === 'development') {
+  // In development mode, use a global variable to ensure that the MongoDB client is only created once
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
+  // In production, create a new client for each request
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
