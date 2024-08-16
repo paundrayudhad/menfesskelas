ed9136cdb3261439a9f190c79ac545e6ee1d0e11
paundrayudhad <155907525+paundrayudhad@users.noreply.github.com>
2024-08-16 14:43:49 +0700

    Update mongodb.js

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,21 +1,20 @@
+// lib/mongodb.js
+
 import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
-const options = {
-  useNewUrlParser: true,
-};
 
 let client;
 let clientPromise;
 
 if (process.env.NODE_ENV === 'development') {
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+    client = new MongoClient(uri, { useNewUrlParser: true });
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  client = new MongoClient(uri, options);
+  client = new MongoClient(uri, { useNewUrlParser: true });
   clientPromise = client.connect();
 }
 
