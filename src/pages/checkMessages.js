48a70ae6d7edf82b8c8f3741f02cfa0d5e289938
paundra461 <paundra461@gmail.com>
2024-08-21 06:26:26 +0000

    .

--- src/pages/checkMessages.js
+++ src/pages/checkMessages.js
@@ -1,5 +1,5 @@
 export async function getServerSideProps() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/messages`);
+  const res = await fetch(`https://portalberita.jeftechjuliversegroup.asia/api/messages.php`);
   const data = await res.json();
   
     return {
@@ -11,10 +11,12 @@ export async function getServerSideProps() {
     return (
       <div class="container mx-auto p-4">
   <h1 class="text-2xl font-bold mb-4">Messages</h1>
-  <table class="min-w-full bg-white border border-gray-300 rounded-lg shadow-md">
+  <table class="min-w-full bg-white border border-gray-300 rounded-lg shadow-md table-stripped">
     <thead>
-      <tr class="bg-gray-200 text-gray-700">
+      <tr class="bg-gray-200 text-gray-700 text-left">
+      <th class="py-2 px-4 border-b">ID</th>
         <th class="py-2 px-4 border-b">To</th>
+        <th class="py-2 px-4 border-b">Pengirim</th>
         <th class="py-2 px-4 border-b">Pesan</th>
         <th class="py-2 px-4 border-b">Musik</th>
       </tr> 
@@ -22,7 +24,9 @@ export async function getServerSideProps() {
     <tbody>
       {data.map((item) => (
         <tr key={item.id} class="hover:bg-gray-100">
-          <td class="py-2 px-4 border-b">{item.to}</td>
+          <td class="py-2 px-4 border-b">{item.id}</td>
+          <td class="py-2 px-4 border-b">{item.ke}</td>
+          <td class="py-2 px-4 border-b">{item.pengirim}</td>
           <td class="py-2 px-4 border-b">{item.pesan}</td>
           <td class="py-2 px-4 border-b">{item.musik}</td>
         </tr>
